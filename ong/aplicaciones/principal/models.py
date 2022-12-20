9b9fccbd3333833033f1be5c3aae1e9a75b18a7d
Dario Benitez <benitezdario007@gmail.com>
2022-12-20 07:14:50 -0300

    se sigue con models

--- ong/aplicaciones/principal/models.py
+++ ong/aplicaciones/principal/models.py
@@ -1,3 +1,7 @@
+from distutils.command.upload import upload
+from pyexpat import model
+from tabnanny import verbose
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
@@ -25,5 +29,29 @@ class Categoria(Principal):
     
     def __str__(self):
         return self.nombre
+
+    
+class Autor(Principal):
+    nombre = models.CharField('Nombres',max_length=100)
+    apellido = models.CharField('Apellido',max_length=100)
+    email = models.EmailField('Email',max_length=150)
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return '{0},{1}'.Format(self.apellido, self.nombre)
+
+class Noticia(Principal):
+    titulo = models.CharField('Titulo de la noticia',max_length=200, unique= True)
+    slug = models.CharField('Slug',max_length=200, unique=True)
+    descripcion = models.TextField('Descripcion')
+    Categoria = models.ForeignKey(Categoria, on_delete= models.CASCADE)
+    autor = models.ForeignKey(Autor, on_delete = models.CASCADE)
+    # imagen = models.ImageField('imagen', upload_to = 'imagenes/', max_length) ver aa¡ca el error
+    
+
+
 
     
